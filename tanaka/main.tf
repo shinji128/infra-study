1d27e4566b0aa781a566f5eca682bd945fe1105e
shzxcv <sh.zxcv00@gmail.com>
2024-12-28 22:23:10 +0900

    add

---
+++ tanaka/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-northeast-1"
+}
+
+resource "aws_ecr_repository" "eks-images" {
+  name = "eks-images"
+}
